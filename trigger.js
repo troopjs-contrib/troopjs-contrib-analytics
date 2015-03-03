d0385e6252c05ae464b68b1e6967afd0a0e20297
Mikael Karon <mikael@karon.se>
2015-03-03 12:06:50 +0800

    Extend trigger to take atributes with string/array value.

--- trigger.js
+++ trigger.js
@@ -3,22 +3,50 @@ define([
   "poly/object",
   "poly/array"
 ], function (Component) {
-  var UNDEFINED;
+  "use strict";
+
+  var OBJECT_TOSTRING = Object.prototype.toString;
+  var TOSTRING_STRING = "[object String]";
+  var TOSTRING_ARRAY = "[object Array]";
+  var TOSTRING_OBJECT = "[object Object]";
+  var TOSTRING_UNDEFINED = "[object Undefined]";
   var $ELEMENT = "$element";
 
   return Component.extend(function ($element, name, triggers) {
     var me = this;
+    var _triggers;
 
-    if (triggers !== UNDEFINED) {
-      me.on("sig/initialize", function () {
-        Object
-          .keys(triggers)
-          .forEach(function (type) {
-            me.on("dom/" + type, function () {
-              me[$ELEMENT].trigger("analytics/trigger", triggers[type]);
-            });
-          });
-      });
+    switch (OBJECT_TOSTRING.call(triggers)) {
+      case TOSTRING_UNDEFINED:
+        return;
+
+      case TOSTRING_STRING:
+        _triggers = {};
+        _triggers[ "click" ] = triggers;
+        break;
+
+      case TOSTRING_ARRAY:
+        _triggers = {};
+        _triggers[ triggers[0] ] = triggers.slice(1);
+        break;
+
+      case TOSTRING_OBJECT:
+        _triggers = triggers;
+        break;
+
+      default:
+        throw new Error("'triggers' is of unsupported type");
     }
+
+    me.on("sig/initialize", function () {
+      Object
+        .keys(_triggers)
+        .forEach(function (type) {
+
+          me.on("dom/" + type, function () {
+            me[$ELEMENT].trigger("analytics/trigger", _triggers[type]);
+          });
+        });
+      });
   });
 });
